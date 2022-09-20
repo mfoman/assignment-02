7c754a94b8d4a2840a64703e3233b1f6d8a9fd1a
William Skou Heidemann <wihe@itu.dk>
2022-09-20 14:12:26 +0200

    implemented 3 anonymous methods

--- Assignment2.Tests/DelegatesTests.cs
+++ Assignment2.Tests/DelegatesTests.cs
@@ -1,10 +1,26 @@
 namespace Assignment2.Tests;
-
+using Assignment2;
 public class DelegatesTests
 {
     [Fact]
-    public void Test1()
+    public void ReverseStringTest()
     {
+        string input = "My test string!";
+        string output = Delegates.Reverse(input);
+        output.Should().Be("!gnirts tset yM");
+    }
 
+    [Fact]
+    public void SquareOf4ShouldBe16()
+    {
+        int input = 4;
+        int output = Delegates.Square(input);
+        output.Should().Be(16);
+    }
+
+    [Fact]
+    public void String_0042ShouldBeInt42()
+    {
+        Delegates.StringToInt(" 0042").Should().Be(42);
     }
 }
---
+++ Assignment2/Delegates.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment2;
+
+public class Delegates
+{
+    public static readonly Func<string, string> Reverse = s => new string(s.ToCharArray().Reverse().ToArray());
+    public static readonly Func<int, int> Square = x => x * x;
+    public static readonly Func<string, int> StringToInt = Int32.Parse;
+}
