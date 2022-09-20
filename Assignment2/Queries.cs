d3659f59ae46d79a250ba5a418271c841f90fbfe
mfoman <frederikblunck@gmail.com>
2022-09-20 14:13:34 +0200

    Make linq work

--- Assignment2/Queries.cs
+++ Assignment2/Queries.cs
@@ -1,5 +1,44 @@
 namespace Assignment2;
 
-public class Queries
+using System.Linq;
+
+public static class Queries
 {
+    public static IEnumerable<String> GetRowlingWizardsNames(IEnumerable<Wizard> wizards)
+    {
+        return from wiz in wizards
+               where wiz.Creator == "Rowling"
+               select wiz.Name;
+    }
+
+    public static int? GetFirstSithLord(IEnumerable<Wizard> wizards)
+    {
+        return (from wiz in wizards
+                where wiz.Medium == "Star Wars" && wiz.Name.StartsWith("Darth")
+                orderby wiz.Year
+                select wiz.Year).First();
+    }
+
+    public static IEnumerable<(string, int?)> GetUniqueFromHarryPotter(IEnumerable<Wizard> wizards)
+    {
+        return (from wiz in wizards
+                where wiz.Medium == "Harry Potter"
+                select (wiz.Name, wiz.Year)).Distinct();
+    }
+
+    public static IEnumerable<(string, string)> ListByCreatorReversed(IEnumerable<Wizard> wizards)
+    {
+        var groups = from wiz in wizards
+                     group wiz by wiz.Creator into g
+                     orderby g.Key descending
+                     select g;
+
+        foreach (var creatorGroup in groups)
+        {
+            foreach (var wiz in creatorGroup)
+            {
+                yield return (wiz.Creator, wiz.Name);
+            }
+        }
+    }
 }
